72863fe150dc228f72b11d6b1b961b535d8e32c0
oselimi <osman.94@live.com>
2021-08-24 23:10:41 +0200

    Testing mailer

--- spec/mailers/applicate_spec.rb
+++ spec/mailers/applicate_spec.rb
@@ -1,32 +1,27 @@
 require "rails_helper"
 
 RSpec.describe ApplicateMailer, type: :mailer do
+  let(:user) { create(:user) }
+  let(:job) { create(:job, user: user) }
+  let(:applicate) { create(:applicate, job: job) }
+
   describe "sent_to_user" do
-    let(:mail) { ApplicateMailer.sent_to_user }
+    let(:mail) { ApplicateMailer.with(applicate: applicate).sent_to_user }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Sent to user")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
-    end
-
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.subject).to eq("New applicant in #{applicate.job.title}")
+      expect(mail.to).to eq([applicate.job.user.email])
+      expect(mail.from).to eq(["noreply@jobonline.com"])
     end
   end
 
   describe "sent_to_applicant" do
-    let(:mail) { ApplicateMailer.sent_to_applicant }
+    let(:mail) { ApplicateMailer.with(applicate: applicate).sent_to_applicant }
 
     it "renders the headers" do
-      expect(mail.subject).to eq("Sent to applicant")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
-    end
-
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+      expect(mail.subject).to eq("Successfully you apply in #{applicate.job.title} ")
+      expect(mail.to).to eq([applicate.email])
+      expect(mail.from).to eq(["noreply@jobonline.com"])
     end
   end
-
 end
--- spec/requests/static_spec.rb
+++ spec/requests/static_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Statics", type: :request do
   describe "GET /index" do
     it "returns http success" do
-      get "/static/index"
+      get "/"
       expect(response).to have_http_status(:success)
     end
   end
